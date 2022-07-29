a739b434edd9140552521873e6b5621e4abac788
AlbusCat <olegkveko@gmail.com>
2022-07-29 15:04:34 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+        
+    }
+
+}
+
+
